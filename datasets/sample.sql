a5a28a8cfbef41fa4f896e1992f5376bacf726e8
Todd Halfpenny <todd@mobilecaddy.net>
2020-05-11 21:00:37 +0100

    Add demo dataset

---
+++ datasets/sample.sql
@@ -0,0 +1,26 @@
+BEGIN TRANSACTION;
+CREATE TABLE "Account" (
+	sf_id VARCHAR(255) NOT NULL, 
+	"Name" VARCHAR(255), 
+	parent_id VARCHAR(255), 
+	PRIMARY KEY (sf_id)
+);
+INSERT INTO "Account" VALUES('0012500001LsqrgAAB','Johnnies Market','');
+CREATE TABLE "Delivery_Item__c" (
+	sf_id VARCHAR(255) NOT NULL, 
+	"Name" VARCHAR(255), 
+	"Food_Expiration_Date__c" VARCHAR(255), 
+	"Food_Storage__c" VARCHAR(255), 
+	delivery__c VARCHAR(255), 
+	PRIMARY KEY (sf_id)
+);
+INSERT INTO "Delivery_Item__c" VALUES('a002500000Aw3daAAB','Coconuts','2020-05-31','Non-refrigerated','a012500000IxrhbAAB');
+CREATE TABLE "Delivery__c" (
+	sf_id VARCHAR(255) NOT NULL, 
+	"Scheduled_Date__c" VARCHAR(255), 
+	"Status__c" VARCHAR(255), 
+	supplier__c VARCHAR(255), 
+	PRIMARY KEY (sf_id)
+);
+INSERT INTO "Delivery__c" VALUES('a012500000IxrhbAAB','2020-05-11T11:00:00.000Z','Completed','0012500001LsqrgAAB');
+COMMIT;
